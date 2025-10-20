f4ba0925dced04aa64dbba9b90c5305086d35499
mami <mamypoco@gmail.com>
2025-10-20 12:10:21 -0700

    added try and except for id route

--- app/models/book.py
+++ app/models/book.py
@@ -8,5 +8,5 @@ books = [
     Book(1, "Fictional Book", "A fantasy novel set in an imaginary world."),
     Book(2, "Wheel of Time", "A fantasy novel set in an imaginary world."),
     Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
-    Book(4, "Novel", "Alice's Adventures in Wonderland")
+    Book(4, "Alice's Adventures in Wonderland", "A historical fantasy novel set in an imaginary world.")
 ]
--- app/routes/book_routes.py
+++ app/routes/book_routes.py
@@ -1,11 +1,9 @@
-from flask import Blueprint
+from flask import Blueprint, make_response, abort
 from app.models.book import books
 
-# You write this first
-books_bp = Blueprint("books_bp", __name__, url_prefix="/")
+books_bp = Blueprint("books_bp", __name__, url_prefix="/books")
 
-# Defining endpoint
-@books_bp.get("/books")
+@books_bp.get("/")
 def get_all_books():
     books_response = []
     for book in books:
@@ -15,3 +13,27 @@ def get_all_books():
             "description": book.description
         })
     return books_response
+
+@books_bp.get("/<book_id>")
+def get_one_book(book_id):
+    book = validate_book(book_id)
+    return {
+        "id": book.id,
+        "title": book.title,
+        "description": book.description
+    }
+
+
+def validate_book(book_id):
+    try:
+        book_id = int(book_id)
+    except:
+        response = {"message": f"book {book_id} is invalid "}
+        abort(make_response(response, 400)) # takes care of python runtime error
+    
+    for book in books: # books is from module at top
+        if book.id == book_id:
+            return book
+        
+    response = {"message": f"book {book_id} is not found"}
+    abort(make_response(response, 404))   
