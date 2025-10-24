1baf7c80d1896c899af6c4cfb016f92b4be29fb8
mami <mamypoco@gmail.com>
2025-10-24 08:03:15 -0700

    started 02 building api - setup modesl is done

--- app/models/book.py
+++ app/models/book.py
@@ -1,12 +1,24 @@
-class Book:
-    def __init__(self, id, title, description):
-        self.id = id
-        self.title = title
-        self.description = description
+from sqlalchemy.orm import Mapped, mapped_column
+from ..db import db
 
-books = [
-    Book(1, "Fictional Book", "A fantasy novel set in an imaginary world."),
-    Book(2, "Wheel of Time", "A fantasy novel set in an imaginary world."),
-    Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
-    Book(4, "Alice's Adventures in Wonderland", "A historical fantasy novel set in an imaginary world.")
-]
+class Book(db.Model):
+    # Mapped will create column?
+    id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
+    title: Mapped[str]
+    description: Mapped[str]
+
+
+
+
+# class Book:
+#     def __init__(self, id, title, description):
+#         self.id = id
+#         self.title = title
+#         self.description = description
+
+# books = [
+#     Book(1, "Fictional Book", "A fantasy novel set in an imaginary world."),
+#     Book(2, "Wheel of Time", "A fantasy novel set in an imaginary world."),
+#     Book(3, "Fictional Book Title", "A fantasy novel set in an imaginary world."),
+#     Book(4, "Alice's Adventures in Wonderland", "A historical fantasy novel set in an imaginary world.")
+# ]
