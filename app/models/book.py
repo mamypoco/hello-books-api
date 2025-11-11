5722cc2c668d95a32d1c7b5a9ae687b6c6f076e9
mami <mamypoco@gmail.com>
2025-11-11 14:30:27 -0800

    finished many to many

--- app/models/book.py
+++ app/models/book.py
@@ -5,6 +5,7 @@ from ..db import db
 
 if TYPE_CHECKING:
     from .author import Author
+    from .genre import Genre
 
 class Book(db.Model):
     id: Mapped[int] = mapped_column(primary_key=True, autoincrement=True)
@@ -12,6 +13,7 @@ class Book(db.Model):
     description: Mapped[str]
     author_id: Mapped[Optional[int]] = mapped_column(ForeignKey("author.id"))
     author: Mapped[Optional["Author"]] = relationship(back_populates="books")
+    genres: Mapped[list["Genre"]] = relationship(secondary="book_genre", back_populates="books")
     # above is schema. If changed above, need migration. 
 
     def to_dict(self):
@@ -22,14 +24,21 @@ class Book(db.Model):
 
         if self.author:
             book_as_dict["author"] = self.author.name
+            
+        if self.genres:
+            book_as_dict['genres'] = [genre.name for genre in self.genres]
 
         return book_as_dict
 
     @classmethod
     def from_dict(cls, book_data):
         author_id = book_data.get("author_id")
+        genres = book_data.get("genres", [])
 
         new_book = cls(
-            title=book_data["title"], description=book_data["description"], author_id=author_id
+            title=book_data["title"], 
+            description=book_data["description"], 
+            author_id=author_id,
+            genres=genres
         )
         return new_book
--- app/routes/genre_routes.py
+++ app/routes/genre_routes.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, make_response, abort, request, Response
 from app.models.genre import Genre
+from app.models.book import Book
 from .route_utilities import validate_model, create_model, get_models_with_filters
 
 bp = Blueprint("genres_bp", __name__, url_prefix="/genres")
@@ -14,13 +15,18 @@ def create_genre():
 def get_all_genres():
     return get_models_with_filters(Genre, request.args)
 
+## added in learn 10)
+@bp.post("/<genre_id>/books")
+def create_book_with_genre(genre_id):
+    genre = validate_model(Genre, genre_id)
+
+    request_body = request.get_json()
+    request_body["genres"] = [genre]
+    return create_model(Book, request_body)
 
 
-# @bp.delete("/<genre_id>")
-# def delete_genre(genre_id):
-#     genre = validate_model(Genre, genre_id)
-    
-#     db.session.delete(genre)
-#     db.session.commit()
-
-#     return Response(status=204, mimetype="application/json")
+@bp.get("/<genre_id>/books")
+def get_books_by_genre(genre_id):
+    genre = validate_model(Genre, genre_id)
+    response = [book.to_dict() for book in genre.books]
+    return response
